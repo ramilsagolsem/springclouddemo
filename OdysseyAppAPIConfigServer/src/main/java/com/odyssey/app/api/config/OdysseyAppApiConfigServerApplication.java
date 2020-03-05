c5a60188d687544c285540ffdf80fc4bfed4d524
Ramil Sagolsem <RAMILS@amdocs.com>
2020-03-05 10:18:25 -0300

    Config Server Commit

---
+++ OdysseyAppAPIConfigServer/src/main/java/com/odyssey/app/api/config/OdysseyAppApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.odyssey.app.api.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdysseyAppApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdysseyAppApiConfigServerApplication.class, args);
+	}
+
+}
