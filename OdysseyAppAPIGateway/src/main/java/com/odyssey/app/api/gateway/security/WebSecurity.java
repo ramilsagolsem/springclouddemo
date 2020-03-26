6fe1f5cf7e4f45cbb4aec48f5dabfe42d1bedd8d
Ramil Sagolsem <ramilsagolsem@gmail.com>
2020-03-26 12:35:22 -0300

    update

--- OdysseyAppAPIGateway/src/main/java/com/odyssey/app/api/gateway/security/WebSecurity.java
+++ OdysseyAppAPIGateway/src/main/java/com/odyssey/app/api/gateway/security/WebSecurity.java
@@ -3,9 +3,11 @@ package com.odyssey.app.api.gateway.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -18,11 +20,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests()
-		.antMatchers("/user/**").permitAll()
-		.antMatchers("/users/**").permitAll()
-		.and()
-		.addFilter(new AuthorizationFilter(authenticationManager(), environment));
 		http.headers().frameOptions().disable();
+		http.authorizeRequests()
+		.antMatchers(environment.getProperty("api.login.actuator.url.path")).permitAll()
+		.antMatchers(environment.getProperty("api.gateway.actuator.url.path")).permitAll()
+		.antMatchers(HttpMethod.POST,environment.getProperty("api.login.url.path")).permitAll()
+		.antMatchers(environment.getProperty("api.login.test.path")).permitAll()
+		.anyRequest().authenticated()
+		.and()
+    	.addFilter(new AuthorizationFilter(authenticationManager(), environment));;
+		
+    	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 }
