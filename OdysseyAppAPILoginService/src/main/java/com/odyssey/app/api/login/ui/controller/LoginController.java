842e38fb940fc1f8c5e54082152be4511b3754f9
Ramil Sagolsem <ramilsagolsem@gmail.com>
2020-03-26 15:01:24 -0300

    update

--- OdysseyAppAPILoginService/src/main/java/com/odyssey/app/api/login/ui/controller/LoginController.java
+++ OdysseyAppAPILoginService/src/main/java/com/odyssey/app/api/login/ui/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.odyssey.app.api.login.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class LoginController {
+	
+	@Autowired
+	private Environment environment;
 
 	@GetMapping("/status/check")
 	public String status() {
-		return "Welcome";
+		return "Running on port: "+environment.getProperty("local.server.port") +" with test data :"+environment.getProperty("config.test.value");
 	}
 }
